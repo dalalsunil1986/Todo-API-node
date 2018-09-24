8165a8f5f035c31ed4794e0738bfd74e0839211b
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-25 01:14:27 +0300

    Find by id

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,7 +8,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').find({ completed: false }).toArray().then(docs => {
+    db.collection('Todos').find({
+        _id: new ObjectID('5ba95fc1173c080f93d8e756')
+    }).toArray().then(docs => {
         console.log('Todos');
         console.log(JSON.stringify(docs, undefined, 2));
     }, err => {
