e738572b17678e32cf30183fe5ee6209e2dae0d0
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-25 00:04:47 +0300

    Insert user into database

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,12 +7,23 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').insertOne({
+    /*db.collection('Todos').insertOne({
         text:'Something to do',
         completed:false
     }, (err, result) => {
         if(err){
             return console.log('Unable to insert to todo', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });*/
+
+    db.collection('Users').insertOne({
+        name: 'John',
+        age: 25,
+        location: 'Earth'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert to users', err);
         }
         console.log(JSON.stringify(result.ops, undefined, 2));
     });
