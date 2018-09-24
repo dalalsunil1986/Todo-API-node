607173672de714c482266e947eab4a0701088ca6
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-25 01:12:47 +0300

    Find by value

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,12 +8,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').find().toArray().then(docs => {
+    db.collection('Todos').find({ completed: false }).toArray().then(docs => {
         console.log('Todos');
         console.log(JSON.stringify(docs, undefined, 2));
     }, err => {
         console.log('Unable to fetch todos', err);
     });
 
-    client.close();
+    //client.close();
 });
