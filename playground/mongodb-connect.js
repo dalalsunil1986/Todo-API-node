493cf8c61c0c4428e87f273e11eda0056608df71
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-24 23:58:05 +0300

    Insert to database. Upgrade to Mongodb v3

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,21 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err) {
         return console.log('Unable to connext to MongoDB');
     }
     console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
 
-    db.close();
+    db.collection('Todos').insertOne({
+        text:'Something to do',
+        completed:false
+    }, (err, result) => {
+        if(err){
+            return console.log('Unable to insert to todo', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    client.close();
 });
