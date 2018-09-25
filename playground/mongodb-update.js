fa61554b20250309e88b9d7358d76c3530e161e7
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-25 23:31:54 +0300

    Increase value with update

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,13 +8,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').findOneAndUpdate(
+    /*db.collection('Todos').findOneAndUpdate(
         {
             _id: new ObjectID('5ba95fc1173c080f93d8e756')
         }, {
             $set: {
                 completed: true
             }
+        }, {
+            returnOriginal: false
+        }).then(result => {
+            console.log(result);
+        });*/
+
+    db.collection('Users').findOneAndUpdate(
+        {
+            _id: new ObjectID('5ba95146522a176261422062')
+        }, {
+            $inc: { age: 1 }
         }, {
             returnOriginal: false
         }).then(result => {
