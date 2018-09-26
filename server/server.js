ce8fbf02a0cf82a75771a173e6fcc750290f5f73
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-26 20:13:04 +0300

    Add POST /todos

--- server/server.js
+++ server/server.js
@@ -6,6 +6,18 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
+app.use(bodyParse.json());
+
+app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    })
+    todo.save().then(doc => {
+        res.status(201).send(doc);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
