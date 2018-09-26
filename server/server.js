a131f58b5a1124218f3f0745f17335feb04322b4
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-26 20:01:41 +0300

    Add express and body-parse

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
-const {mongoose} = require('./db/mongoose');
-const {Todo} =  require('./models/todo');
-const {User} = require('./models/user');
+const express = require('express');
+const bodyParse = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
