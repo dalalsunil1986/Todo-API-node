edb30b938dbaae2cd0c5dbde475346d2c195e094
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-09-30 23:02:46 +0300

    Add delete todo by id

--- server/server.js
+++ server/server.js
@@ -45,6 +45,22 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then(todo => {
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(e => {
+        res.status(400).send();
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
