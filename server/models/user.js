6e71fde7811e9baa0819c45a129f2535e7829f13
Jyendor <34946396+Jyendor@users.noreply.github.com>
2018-10-08 21:32:40 +0300

    Update user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
-})
+        trim: true,
+        unique: true,
+        validate: {
+            validator: isEmail,
+            message: '{VALUE} is noy a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minLength: 8
+    },
+    token: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
 
 module.exports = { User };
